8f0af0b39a4a71a275897aaa4899bd2bd967bd56
shahid2107 <mshahid2002shaikh@gmail.com>
2023-09-16 12:53:45 -0400

    Base Sequence Added

--- Development/SRC/sequences/ei_tdp_ram_base_sequence.sv
+++ Development/SRC/sequences/ei_tdp_ram_base_sequence.sv
@@ -1,7 +1,7 @@
 /******************************************************************/ 
 /*----------------------------------------------------------------*/ 
-/* File name      : ei_tdp_ram_sanity_sequence.sv                 */ 
-/* Title          : Sanity Sequence for TDP RAM UVC               */ 
+/* File name      : ei_tdp_ram_base_sequence.sv                   */ 
+/* Title          : Base Sequence for TDP RAM UVC                 */ 
 /* Project        : TDP RAM UVC                                   */ 
 /* Created on     : 16/09/2023                                    */ 
 /* Developer      : Mahammadshahid Shaikh                         */ 
@@ -10,21 +10,21 @@
 /*----------------------------------------------------------------*/ 
 
 //class declaration
-class ei_tdp_ram_sanity_sequence_c extends ei_tdp_ram_base_sequence_c;
+class ei_tdp_ram_base_sequence_c extends uvm_sequence #(ei_tdp_ram_seq_item_c);
 
     //factory registration of the class
-    `uvm_object_utils(ei_tdp_ram_sanity_sequence_c)
+    `uvm_object_utils(ei_tdp_ram_base_sequence_c)
 
     //variable to store local address
     local bit [`ADDR_WIDTH - 1 : 0] address;
 
     //user-defined constructor declaration
-    extern function new(string name = "sanity_sequence_h");
+    extern function new(string name = "base_sequence_h");
 
     //body method declaration
     extern task body();
 
-endclass : ei_tdp_ram_sanity_sequence_c
+endclass : ei_tdp_ram_base_sequence_c
 
 ////////////////////////////////////////////////////////////////////////
 //Method name         : new 
@@ -33,7 +33,7 @@ endclass : ei_tdp_ram_sanity_sequence_c
 //Description         : User-defined constructor to allocate memory
 ////////////////////////////////////////////////////////////////////////
  
-function ei_tdp_ram_sanity_sequence_c::new(string name = "sanity_sequence_h");
+function ei_tdp_ram_base_sequence_c::new(string name = "sanity_sequence_h");
      
     //calling parent class constructor 
     super.new(name); 
@@ -47,16 +47,9 @@ endfunction : new
 //Description         : Generate and send the transaction to sequencer
 ////////////////////////////////////////////////////////////////////////
 
-task ei_tdp_ram_sanity_sequence_c::body();
+task ei_tdp_ram_base_sequence_c::body();
 
-    //allocating the memory to tr_h
-    tr_h = ei_tdp_ram_seq_item_c::type_id::create("tr_h");
-
-    //starting the sequence for write operation
-    `uvm_do_with(tr_h, {we_a == 1'b1; we_b == 1'b0; re_a == 1'b0; re_b == 1'b0;})
-    //storing the address
-    address = tr_h.addr_a;
-    //starting the sequence for read operation
-    `uvm_do_with(tr_h, {we_a == 1'b0; we_b == 1'b0; re_a == 1'b1; re_b == 1'b0; addr_a == address;})
+    `uvm_info("Base Sequence", "Inside Body", UVM_FULL)
 
 endtask : body
+
